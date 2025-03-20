65e45c28326ff56a36e868c0d2a02c06ef7975f5
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-03-20 11:19:21 +0530

    [Shubham Rajput] Added : Spring basics

---
+++ Spring/src/main/java/com/shubham/SpringDemo/Computer.java
@@ -0,0 +1,5 @@
+package com.shubham.SpringDemo;
+
+public interface Computer {
+     void compile();
+}
---
+++ Spring/src/main/java/com/shubham/SpringDemo/Laptop.java
@@ -0,0 +1,8 @@
+package com.shubham.SpringDemo;
+
+public class Laptop implements Computer {
+
+    public void compile(){
+        System.out.println("Compiling laptop...");
+    }
+}
